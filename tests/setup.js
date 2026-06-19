4a6c75576187ed7cfb4d4184b5cb1e811d964077
Laxmikant Rokade <laxmikantrokade474@gmail.com>
2026-06-19 19:03:53 +0530

    chore: clean eslint configuration and code quality issues

--- tests/setup.js
+++ tests/setup.js
@@ -23,7 +23,7 @@ Object.defineProperty(window, 'matchMedia', {
 });
 
 // Mock ResizeObserver
-global.ResizeObserver = class ResizeObserver {
+globalThis.ResizeObserver = class ResizeObserver {
   observe() {}
   unobserve() {}
   disconnect() {}
